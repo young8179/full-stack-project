3b4640d99423fa32904219f0cf65f10bc168a8da
Bargavi  Kanneganti <bargavikanneganti@Bargavis-MBP.grandenetworks.net>
2020-10-15 13:37:00 -0500

    updated index files

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const bcrypt = require('bcrypt');
+const db = require('../models')
 
 // router.get('/register', function(req, res, next) {
 //     res.send('respond with a resource');
